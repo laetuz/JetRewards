8dcf9f958fcf319ea08c7dacd8774d6b1a2af6ba
Ryo Martin <laetuzg@gmail.com>
2023-05-14 00:25:54 +0700

    Added LazyGrid to HomeScreen.

--- app/src/main/java/com/neotica/jetrewards/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/neotica/jetrewards/ui/screen/home/HomeScreen.kt
@@ -1,22 +1,56 @@
 package com.neotica.jetrewards.ui.screen.home
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import com.neotica.jetrewards.R
+import androidx.compose.ui.unit.dp
+import com.neotica.jetrewards.model.OrderReward
+import com.neotica.jetrewards.ui.common.UiState
+import com.neotica.jetrewards.ui.components.RewardItem
+import org.koin.androidx.compose.getViewModel
 
 @Composable
 fun HomeScreen(
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = getViewModel()
+) {
+    viewModel.uiState.collectAsState(initial = UiState.Loading).value.let {
+        when (it) {
+            is UiState.Loading -> {viewModel.getAllRewards()}
+            is UiState.Success -> {
+                HomeContent(orderReward = it.data, modifier = modifier)
+            }
+            is UiState.Error -> {it.errorMessage}
+        }
+    }
+}
+
+@Composable
+fun HomeContent(
+    orderReward: List<OrderReward>,
     modifier: Modifier = Modifier
 ) {
-    Box(
-        modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center,
+    val lazyGridState = rememberLazyGridState()
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(160.dp),
+        state = lazyGridState,
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        Text(stringResource(R.string.menu_home))
+        items(orderReward) {
+            RewardItem(
+                image = it.reward.image,
+                title = it.reward.title,
+                requiredPoint = it.reward.requiredPoint
+            )
+        }
     }
 }
